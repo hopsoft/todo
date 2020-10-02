e35c47bea9718b31b24ce10e777eb2cb34eb747c
Hopsoft <natehop@gmail.com>
2020-10-01 21:19:01 -0600

    Setup shoestring web components and stub main page

---
+++ db/migrate/20201002000924_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :session_id, null: false
+      t.string :title, null: false
+      t.boolean :completed, null: false, default: false
+      t.timestamps
+
+      t.index :session_id
+      t.index :completed
+    end
+  end
+end
